4d5e65485bd295677c97f3b8fd8349062fce2b59
Theo Richart <theor@unity3d.com>
2023-03-08 10:38:50 -0500

    tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,8 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     banner: z.string().optional(),
+    abstract: z.string().optional(),
+    tags: z.string().array().optional(),
     draft: z.boolean().optional(),
     // Transform string to Date object
     pubDate: z
