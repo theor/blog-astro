f75aa7b142737313bf3ee595ab923bfde8b04318
Theo Richart <theor@unity3d.com>
2023-03-14 12:57:45 -0400

    headings links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import m2dx from "astro-m2dx";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 import sitemap from "@astrojs/sitemap";
-import {myAstro} from './src/integration'
+import {myAstro} from './src/integration';
 
 /** @type {import('astro-m2dx').Options} */
 const m2dxOptions = {
@@ -12,6 +14,12 @@ const m2dxOptions = {
   // doesn't work with astro getCollection
   // scanAbstract: true,
   // rawmdx: true,
+};
+/** @type {import('rehype-autolink-headings').Options} */
+const headingsOptions = {
+behavior: "prepend",
+// content: h('span', 'test'),
+properties: {"data-link":true}
 };
 
 // https://astro.build/config
@@ -23,6 +31,7 @@ export default defineConfig({
   integrations: [myAstro(), mdx(), sitemap(), ],
   markdown: {
     remarkPlugins: [[m2dx, m2dxOptions]],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, headingsOptions]],
     extendDefaultPlugins: true,
   },
 });
