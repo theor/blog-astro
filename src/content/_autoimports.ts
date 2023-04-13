6d92ecc472223e4085491326066392df27f44565
Theo Richart <theor@unity3d.com>
2023-04-13 11:04:59 -0400

    just missing the laser cutting

--- src/content/_autoimports.ts
+++ src/content/_autoimports.ts
@@ -1,9 +1,11 @@
 import Video from "../components/Video.astro";
 import Box from "../components/Box.astro";
+import Figure from "../components/Figure.astro";
 import { YouTube } from "@astro-community/astro-embed-youtube";
 
 export const autoimports = {
   Video,
   YouTube,
   Box,
+  Figure,
 };
