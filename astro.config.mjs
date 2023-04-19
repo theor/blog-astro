e39276bd641b42e09d28c53d6b44151d417e88d3
Theo Richart <theor@unity3d.com>
2023-04-19 16:16:43 -0400

    lazy load all the things

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import {myAstro} from './src/integration';
 const m2dxOptions = {
   relativeImages: true,
   autoImports: true,
+  exportComponents: true,
   // doesn't work with astro getCollection
   // scanAbstract: true,
   // rawmdx: true,
